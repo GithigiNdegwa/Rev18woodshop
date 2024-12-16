5f3a0cb1cd6d53ea892cc59583c6009879f41f94
GithigiNdegwa <153816703+GithigiNdegwa@users.noreply.github.com>
2024-12-16 03:23:07 +0300

    Create products.js

---
+++ products.js
@@ -0,0 +1,34 @@
+const products = [
+    {
+        id: "CW001",
+        name: "Citron Wood King Bed Frame",
+        price: 4850.00,
+        currency: "EUR",
+        origin: "Kenya",
+        description: "Handcrafted king-size bed frame with citron wood's natural citrus fragrance",
+    },
+    {
+        id: "CW002",
+        name: "Citron Wood Floating Shelves Set",
+        price: 780.00,
+        currency: "EUR",
+        origin: "Congo",
+        description: "Set of 3 floating shelves with hidden brackets",
+    },
+    {
+        id: "MH001",
+        name: "Mahogany Executive Desk",
+        price: 3200.00,
+        currency: "EUR",
+        origin: "DRC",
+        description: "Executive desk with leather inlay",
+    },
+    {
+        id: "SW001",
+        name: "Lemon Thyme Wood Console",
+        price: 1950.00,
+        currency: "EUR",
+        origin: "Kenya",
+        description: "Console table with natural citrus-herb fragrance",
+    }
+];
